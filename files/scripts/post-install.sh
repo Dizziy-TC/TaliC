a04fd53e2c3b4e07dec1f45c79a6d811544ffb8d
Dizziy-TC <anomyrgieseke@gmail.com>
2026-04-18 11:13:49 +0200

    Update post-install.sh

--- files/scripts/post-install.sh
+++ files/scripts/post-install.sh
@@ -9,3 +9,6 @@ echo "Starte TaliC OS Optimierungen..."
 # Hier kannst du später Befehle einfügen, die nach der Installation laufen sollen.
 
 echo "Optimierungen abgeschlossen!"
+
+# Setzt das TaliC Logo als Icon für das Startmenü (KDE Kickoff)
+sed -i 's/icon=fedora-logo-icon/icon=talic-logo/' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml || true
