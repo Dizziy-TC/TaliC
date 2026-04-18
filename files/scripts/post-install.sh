1219ad8b51418a142d1b2faa9695fd9ac2c002d4
Dizziy-TC <anomyrgieseke@gmail.com>
2026-04-18 11:33:06 +0200

    Update post-install.sh

--- files/scripts/post-install.sh
+++ files/scripts/post-install.sh
@@ -12,3 +12,6 @@ echo "Optimierungen abgeschlossen!"
 
 # Setzt das TaliC Logo als Icon für das Startmenü (KDE Kickoff)
 sed -i 's/icon=fedora-logo-icon/icon=talic-logo/' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml || true
+
+# Setzt das Standard-Design für den Boot-Vorgang
+plymouth-set-default-theme bgrt -R
