3b86cf09c26da21eac2c8245c686a8c158236131
Dizziy-TC <anomyrgieseke@gmail.com>
2026-04-18 10:50:53 +0200

    Create post-install.sh

---
+++ files/scripts/post-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Fehler abfangen
+set -oue pipefail
+
+echo "Starte TaliC OS Optimierungen..."
+
+# Beispiel: Starte den Steam-Hardware-Check oder ähnliches
+# Hier kannst du später Befehle einfügen, die nach der Installation laufen sollen.
+
+echo "Optimierungen abgeschlossen!"
